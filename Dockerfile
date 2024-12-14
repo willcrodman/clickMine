a37b419af0c1245bd0d80d18f7723be94d3441c6
Will Rodman <willc.rodman@gmail.com>
2024-12-14 13:58:18 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Expose port 8080 (adjust as needed)
-EXPOSE 8080
+EXPOSE 80
 
 # Command to run the app
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def serve_index(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
